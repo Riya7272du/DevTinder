6d1f95a022a5e1ded5045d9c74232ba9d6d95159
riya7272 <riya.2022ca083@mnnit.ac.in>
2025-01-20 11:29:07 +0530

    updated the base

--- src/app.js
+++ src/app.js
@@ -8,10 +8,7 @@ const cookieParser = require('cookie-parser');
 const jwt = require("jsonwebtoken");
 const { userAuth } = require("./middleware/auth");
 const cors = require("cors");
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
